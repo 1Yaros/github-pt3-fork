da74ba330f60c8b922990afce31a10f494531804
Yaros <yaroslav.mieshcheriakov@cirvianum.cat>
2024-12-12 09:44:44 +0100

    Actualitzades dades del fitxer Program.cs amb vehicles Tor

--- base/Program.cs
+++ base/Program.cs
@@ -6,13 +6,13 @@ class Program
     static void Main(string[] args)
     {
         // Crear un objecte Vehicle
-        Vehicle vehicle1 = new Vehicle("Model S", "Tesla", 2022, 250.0);
+        Vehicle vehicle1 = new Vehicle("Model A", "Tor", 2024, 1199.0);
 
         // Mostrar la informació del vehicle
         vehicle1.MostrarInformacio();
 
         // Crear un altre objecte Vehicle
-        Vehicle vehicle2 = new Vehicle("Mustang", "Tesla", 1965, 220.0);
+        Vehicle vehicle2 = new Vehicle("Model S", "Tor", 1982, 999.0);
         
         // Mostrar la informació del segon vehicle
         vehicle2.MostrarInformacio();
