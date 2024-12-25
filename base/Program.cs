03258f5fdd3b5742c7904e1e3ad4568d7984cbc2
Yaros <yaroslav.mieshcheriakov@cirvianum.cat>
2024-12-25 20:08:37 +0100

    Tot arreglat, disculpeu les molèsties

--- base/Program.cs
+++ base/Program.cs
@@ -6,13 +6,13 @@ class Program
     static void Main(string[] args)
     {
         // Crear un objecte Vehicle
-        Vehicle vehicle1 = new Vehicle("Model A", "Tor", 2024, 1199.0);
+        Vehicle vehicle1 = new Cotxe("Model A", "Tor", 2024, 1199.0, 4);
 
         // Mostrar la informació del vehicle
         vehicle1.MostrarInformacio();
 
         // Crear un altre objecte Vehicle
-        Vehicle vehicle2 = new Vehicle("Model S", "Tor", 1982, 999.0);
+        Vehicle vehicle2 = new Cotxe("Model S", "Tor", 1982, 999.0, 4);
         
         // Mostrar la informació del segon vehicle
         vehicle2.MostrarInformacio();
